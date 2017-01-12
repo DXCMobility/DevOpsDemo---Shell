3e7833f78f9c2eec8666ad8693e99b04eba3e82c
Gokul Nath Periasamy <gokul-nath.periasamy@hpe.com>
2017-01-12 19:54:56 +0530

    Initial Check in - Shell

---
+++ app/src/main/java/com/hpe/devops/activity/LauncherActivity.java
@@ -0,0 +1,28 @@
+package com.hpe.devops.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import com.hpe.devops.R;
+import com.hpe.devops.application.DevOpsApplication;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+        DevOpsApplication.inject(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        return true;
+    }
+}
